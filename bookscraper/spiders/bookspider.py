4e18f9e03763c72da735c89561aefded94ce3d7b
Kamal <kamal.farahani90@gmail.com>
2025-01-04 18:17:51 +0330

    Complete until part 5

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -7,4 +7,25 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
+        books = response.css("article.product_pod")
+
+        for book in books:
+            book_url = book.css("h3 a").attrib["href"]
+            yield response.follow(
+                book_url,
+                callback=self.parse_book_page,
+            )
+
+        try:
+            next_page = response.css("li.next a").attrib["href"]
+        except KeyError:
+            next_page = None
+
+        if next_page is not None:
+            yield response.follow(
+                next_page,
+                callback=self.parse,
+            )
+
+    def parse_book_page(self, response):
         pass
